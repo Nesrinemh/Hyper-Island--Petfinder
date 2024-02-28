2267da915ae644a2caa64470dfc45b3a00d82bc7
Nesrinemh <nesrine.hadboun@live.fr>
2024-02-28 18:50:46 +0100

    commit new bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-
-import React from "react";
-import Hero from "./components/Hero";
-import Header from "./components/Header";
-import Pets from "./components/Pets.jsx";
-import Popup from "./components/Popup";
-import Reviews from "./components/Reviews";
-import AboutUs from "./components/AboutUs";
-import Footer from "./components/Footer";
-import Process from "./components/Process";
-
+import Hero from './components/Hero';
+import Header from './components/Header';
+import Pets from './components/Pets.jsx';
+import Popup from './components/Popup';
+import Reviews from './components/Reviews';
+import AboutUs from './components/AboutUs';
+import Footer from './components/Footer';
+import Process from './components/Process';
 
 export default function App() {
   return (
     <>
-
-
       <Header />
-      <main className='p-6 md:p-10 flex flex-col gap-[5rem]'>
+      <main className='p-6 md:p-10 flex flex-col gap-[5rem] sm:gap-[8rem] mx-auto lg:max-w-7xl'>
         <Hero />
         <Pets />
         <Process />
